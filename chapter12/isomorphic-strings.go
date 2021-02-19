412caa4b4724c8af3edb24ffce0ddad32012744a
zhanghaojie <zhanghaojie@xiaoniangao.com>
2021-02-19 11:37:16 +0800

    ADD: 增加 chapter12

---
+++ chapter12/isomorphic-strings.go
@@ -0,0 +1,22 @@
+package main
+
+func isIsomorphic(s string, t string) bool {
+	s2t := make(map[byte]byte)
+	t2s := make(map[byte]byte)
+
+	for i := range s {
+		x, y := s[i], t[i]
+		if s2t[x] > 0 && s2t[x] != y || t2s[y] > 0 && t2s[y] != x {
+			return false
+		}
+
+		s2t[x] = y
+		t2s[y] = x
+	}
+
+	return true
+}
+
+func main() {
+	println(isIsomorphic("egg", "add"))
+}
---
+++ chapter12/valid-anagram.go
@@ -0,0 +1,18 @@
+package main
+
+func isAnagram(s string, t string) bool {
+	var c1, c2 [26]int
+	for _, ch := range s {
+		c1[ch-'a']++
+	}
+
+	for _, ch := range t {
+		c2[ch-'a']++
+	}
+
+	return c1 == c2
+}
+
+func main() {
+	println(isAnagram("anagram", "nagaram"))
+}
